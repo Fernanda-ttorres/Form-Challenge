055379ccad53e39f7fbe762c00dfb256ddbd2769
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-06 15:32:21 -0300

    Feat: crating the title component

---
+++ form-challenge/src/Components/Micro/Title/Title.styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const TagH2Styled = styled.h2`
+    font-family: 'Inter', sans-serif;
+    font-weight: 700;
+    font-size: 2.5rem;
+    color: #074EE8;
+    line-height: 3rem;
+    margin-block: 0;
+`;
+
+export const TagH5Styled = styled.h5`
+    font-family: 'Inter', sans-serif;
+    font-weight: 600;
+    font-size: 1.125rem;
+    color: #111111;
+    line-height: 1.5rem;
+    margin-block: 0;
+`;
