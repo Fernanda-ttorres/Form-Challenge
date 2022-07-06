dc9012aca3dc5b4d18acb4526a0902ecf36e49ca
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-06 13:59:38 -0300

    Fix: adding a path to the input component

--- form-challenge/src/App.jsx
+++ form-challenge/src/App.jsx
@@ -1,9 +1,6 @@
-import { Input } from "./Components/Micro/Input"
-
 function App() {
   return (
     <>
-    <Input type="text" label="Full Name *" placeholder="Foo Bar" isError="Please enter your name"/>
     </>
   )
 }
