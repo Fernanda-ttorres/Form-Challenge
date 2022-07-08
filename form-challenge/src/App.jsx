bcba5465fb0415c63264de0de62a8a17b0ce9118
Joao Augusto <joaoaugustofrezende@outlook.com>
2022-07-08 19:39:33 -0300

    refactor: clear app page

--- form-challenge/src/App.jsx
+++ form-challenge/src/App.jsx
@@ -1,10 +1,6 @@
-import Button from "./Components/Button/Button"
-import Image from "./Assets/check.png"
-import ImageTwo from "./Assets/right-arrow.png"
-
 function App() {
   return (
-    <> <Button id="finish" text="Finish" image={Image}/> </>
+    <></>
   )
 }
 
