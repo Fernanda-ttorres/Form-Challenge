b29d7661c2fa48fb306ca6e1983521b4a516b9f4
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-08 10:51:35 -0300

    Feat: creating the micro component label

---
+++ form-challenge/src/Components/Micro/Label/Label.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {LabelStyled} from './Label.styled';
+
+export const Label = ({label}) => {
+    return (
+     <LabelStyled>{label}</LabelStyled>
+    );
+};
---
+++ form-challenge/src/Components/Micro/Label/Label.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const LabelStyled = styled.label`
+    font-family: 'Nunito', sans-serif;
+    font-size: 0.875rem;
+    line-height: 1rem;
+    font-weight: 400;
+    color: #767676;    
+`;
