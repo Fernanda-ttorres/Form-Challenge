7d972387da91a106b49afceb323551ef07b585e8
fernando <fernandoreal.valentim@gmail.com>
2022-07-08 12:30:19 -0300

    Fix: fixed props

--- form-challenge/src/Components/Micro/Checkbox/Checkbox.jsx
+++ form-challenge/src/Components/Micro/Checkbox/Checkbox.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { CheckStyled } from "./Checkbox.styled";
 
-export const CheckBox = () => {
+export const CheckBox = ({label, error}) => {
   return (
     <CheckStyled>
       <div className="checkbox-div">
         <input type="checkbox" className="checkbox" />
-        <label htmlFor="checkbox">I accept the terms and privacy</label>
-        <p className="error">Please confirm the terms</p>
+        <label htmlFor="checkbox">{label}</label>
+        <p className="error">{error}</p>
       </div>
       
     </CheckStyled>
