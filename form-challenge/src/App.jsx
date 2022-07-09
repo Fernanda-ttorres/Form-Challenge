f703f698470af47c7e2e7638916878311404892d
fernando <fernandoreal.valentim@gmail.com>
2022-07-09 16:57:57 -0300

    Tests

--- form-challenge/src/App.jsx
+++ form-challenge/src/App.jsx
@@ -1,8 +1,25 @@
-import Navbar from "./Components/Micro/Navbar/Navbar"
+import Navbar from "./Components/Micro/Navbar/Navbar";
+import React from "react";
+import { useState } from "react";
+
+export const AuthContext = React.createContext();
+
+
 function App() {
+ 
+  let [isLogged, setIsLogged] = useState(false);
+
+  function permission() {
+    setIsLogged((isLogged = true));
+    console.log("a")
+  }
+
+
   return (
-    <><Navbar/></>
-  )
+    <AuthContext.Provider value={permission}>
+      <Navbar />
+    </AuthContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- form-challenge/src/Components/Micro/Navbar/Navbar.jsx
+++ form-challenge/src/Components/Micro/Navbar/Navbar.jsx
@@ -1,56 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { NavStyled } from "./Navbar.styled";
 
+import { AuthContext } from "../../../App";
+
 export const Navbar = () => {
- 
-  const [a, SetA] = useState("false");
-  const [b, SetB] = useState("false");
-  const [c, SetC] = useState("false");
+  
+
+  const [a, SetA] = useState(false);
+  const [b, SetB] = useState(true);
+  const [c, SetC] = useState(true);
 
   
-function settingA() {
-  SetA(current => !current);
-  SetB(true);
-  SetC(true);
 
-  
-}
-
-function settingB() {
-  SetA(true);
-  SetB(current => !current);
-  SetC(true);
-}
-
-function settingC() {
-  SetA(true);
-  SetB(true);
-  SetC(current => !current);
-}
+  function setTab(x, y, z) {
+    SetA(x);
+    SetB(y);
+    SetC(z);
+  }
 
 
-  function handleClick(value) {
-    switch(value) {
-      case "basic": settingA();
-      break;
-      case "social": settingB();
-      break;
-      case "certificates": settingC();
-      break;
+  function handleClick(value){
+    if(value == "basic") {
+      setTab(false, true, true);
+    } else if (value == "social") {
+      setTab(true, false, true);
+    } else if ( value == "certificates") {
+      setTab(true, true, false);
     }
   }
 
   const setName = (x) => {
-    return (x ? 'false' : 'true');
-   }
+    return x ? "false" : "true";
+  };
+
+  
+  const setContext = useContext(AuthContext);
 
   return (
     <NavStyled>
+      <button className="one" onClick={() => setContext()}>|</button>
+      <button className="two" onClick={() => setContext()}>||</button>
+
       <div className="navbar-div">
         <nav className="nav">
-          <p name="basic" className={setName(a)}  onClick={() => handleClick("basic")}>Basic</p>
-          <p name="basic" className={setName(b)} onClick={() => handleClick("social")}> Social</p>
-          <p name="basic"className={setName(c)} onClick={() => handleClick("certificates")}>Certificates</p>
+          <p
+            name="basic"
+            className={setName(a)}
+            onClick={() => handleClick("basic")}
+          >
+            Basic
+          </p>
+          <p
+            name="basic"
+            className={setName(b)}
+            onClick={() => handleClick("social")}
+          >
+            {" "}
+            Social
+          </p>
+          <p
+            name="basic"
+            className={setName(c)}
+            onClick={() => handleClick("certificates")}
+          >
+            Certificates
+          </p>
         </nav>
       </div>
     </NavStyled>
