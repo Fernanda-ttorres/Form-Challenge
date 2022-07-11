76e63c5a49597abf773fc373a0d5105d5f9b39fd
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-11 11:07:40 -0300

    Refactor: creating the input component with the micro components

---
+++ form-challenge/src/Components/Input/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Label} from '../Micro/Label/Label'
+import InputTag from '../Micro/InputTag/InputTag'
+import { InputContainer } from './Input.styled';
+
+export const Input = ({id, label, type, placeholder, register}) => {
+    return (
+        <InputContainer >
+            <Label id={id} label={label}/>
+            <InputTag className={`${id}`}  id={id} type={type} placeholder={placeholder} rgister={register}/>
+        </InputContainer>
+    );
+};
---
+++ form-challenge/src/Components/Input/Input.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 0.25rem;
+`;
+
+
+
