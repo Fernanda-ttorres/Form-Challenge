f0ca9817450636ef15314802fb80751a80164cad
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-12 16:47:12 -0300

    Fix: changing the id of the inputs

--- form-challenge/src/Components/Graduation/Graduation.jsx
+++ form-challenge/src/Components/Graduation/Graduation.jsx
@@ -4,9 +4,9 @@ import { GraduationDiv } from './Graduation.styled';
 export const Graduation = ({register}) => {
     return (
         <GraduationDiv>
-           <Input id="form" type="text"  label="Team Name *" placeholder="https://www.linkedin.com/in/foo-bar-3a0560104/" register={register} />
-           <Input id="form" type="text"  label="Institution *" placeholder="Universidade Federal da Paraíba" register={register} />
-           <Input id="form" type="text"  label="Graduation *" placeholder="Ciências da Computação" register={register} />
+           <Input id="team" type="text"  label="Team Name *" placeholder="https://www.linkedin.com/in/foo-bar-3a0560104/" register={register} />
+           <Input id="institution" type="text"  label="Institution *" placeholder="Universidade Federal da Paraíba" register={register} />
+           <Input id="graduation" type="text"  label="Graduation *" placeholder="Ciências da Computação" register={register} />
         </GraduationDiv>
     );
 };
