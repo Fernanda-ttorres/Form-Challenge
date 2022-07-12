5ef23b83ffd9653710f79ab416e85438d4536fbf
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-12 16:40:40 -0300

    Fix: changing the id of the inputs

--- form-challenge/src/Components/Contact/Contact.jsx
+++ form-challenge/src/Components/Contact/Contact.jsx
@@ -5,8 +5,8 @@ import { ContactDiv } from './Contact.styled';
 export const Contact = ({register}) => {
     return (
         <ContactDiv>
-           <Input id="form" type="email"  label="Email *" placeholder="foor@bar.com" register={register} />
-           <Input id="form" type="tel"  label="Phone" placeholder="(83) 00000-0000" register={register} />
+           <Input id="email" type="email"  label="Email *" placeholder="foor@bar.com" register={register} />
+           <Input id="phone" type="tel"  label="Phone" placeholder="(83) 00000-0000" register={register} />
         </ContactDiv>
     );
 };
