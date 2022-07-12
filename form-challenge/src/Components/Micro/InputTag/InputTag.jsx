c4b8b98218768d1534db378087f22aaaf3a0ce47
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-12 14:10:59 -0300

    Fix: changing the className prop

--- form-challenge/src/Components/Micro/InputTag/InputTag.jsx
+++ form-challenge/src/Components/Micro/InputTag/InputTag.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import  {InputTagStyled}  from './InputTag.styled';
 
-const InputTag = ({id, type, placeholder, register}) => {
+const InputTag = ({style, id, type, placeholder, register}) => {
     return (
-    <InputTagStyled className={`${id}`}  id={id} type={type} placeholder={placeholder} {...register(id)} />
+    <InputTagStyled className={style} id={id} type={type} placeholder={placeholder} {...register(id)} />
     );
 };
 
