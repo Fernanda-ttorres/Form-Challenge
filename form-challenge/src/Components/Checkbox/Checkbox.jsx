3195c6141e32e69e0b833c3bf6ec490515bb6a23
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-13 17:56:22 -0300

    Fix: editing the className prop

--- form-challenge/src/Components/Checkbox/Checkbox.jsx
+++ form-challenge/src/Components/Checkbox/Checkbox.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import InputTag from "../Micro/InputTag/InputTag";
 import { Label } from "../Micro/Label/Label";
 import { CheckBoxStyled } from "./Checkbox.styled";
-import { useForm } from "react-hook-form";
 
-const CheckBox = ({ id, type, container, label}) => {
-  const { register, handleSubmit, formState: { errors }, reset } = useForm();
-    
+const CheckBox = ({ id, type, register, label}) => {    
   return (
     <CheckBoxStyled>
-      <InputTag className={id} id={id} type={type} register={register}/>
-      <Label id={id} label={label}/>
+      <InputTag style="checkbox" id={id} type={type} register={register}/>
+      <Label style="checkbox" label={label}/>
     </CheckBoxStyled>
   );
 };
