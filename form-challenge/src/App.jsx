4c41842525d5bc54919bdd0f06ef82474d4929ed
fernando <fernandoreal.valentim@gmail.com>
2022-07-13 19:22:05 -0300

    Fix: removed app imports

--- form-challenge/src/App.jsx
+++ form-challenge/src/App.jsx
@@ -1,14 +1,5 @@
-import Navbar from "./Components/Micro/Navbar/Navbar";
-import React from "react";
-import { useState } from "react";
-
-export const AuthContext = React.createContext();
-
-
 function App() {
-  let [page, setPage] = useState(false);
-  
-  return 
+  return<></>;
 }
 
 export default App;
