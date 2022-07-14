fa4f86263ce5453db7936362e9d10e9479933531
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-14 14:11:26 -0300

    Feat: removing the navbar

--- form-challenge/src/Components/Card/Card.jsx
+++ form-challenge/src/Components/Card/Card.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { CardStyled } from "./Card.styled";
-import Navbar from "../Micro/Navbar/Navbar";
 import {Title} from "../Micro/Title/Title"
-export const Card = () => {
+
+const Card = () => {
   return (
     <CardStyled>
       <Title tagName={"h5"} text={"Team Sign up"}/>
-      <Navbar />
     </CardStyled>
   );
 };
 
-export default Text;
+export default Card;
