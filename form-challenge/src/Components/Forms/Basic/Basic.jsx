de5136fea110d6c3bf8aebeef0db3677c2496d2e
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-17 13:37:11 -0300

    Feat: adding the birthday component

--- form-challenge/src/Components/Forms/Basic/Basic.jsx
+++ form-challenge/src/Components/Forms/Basic/Basic.jsx
@@ -1,13 +1,13 @@
-import { useForm } from  "react-hook-form";
 import { Names } from "../../Names/Names";
 import {Contact} from "../../Contact/Contact";
-import Checkbox from "../../Checkbox/Checkbox"
+import Checkbox from "../../Checkbox/Checkbox";
 import { BasicForm, DivBtn, DivCheck } from "./Basic.styled";
 import Button from '../../Button/Button';
-import Image from '../../../Assets/right-arrow.png'
+import Image from '../../../Assets/right-arrow.png';
+import Birthday from '../../Birthday/Birthday';
 
 const Basic = () => {
-    const { register, handleSubmit, formState: { errors }, reset } =  useForm();
+    
     return (
         <BasicForm>
             <Names register={register} />
@@ -15,6 +15,7 @@ const Basic = () => {
             <DivCheck>
                 <Checkbox id="checkbox" type="checkbox" label="I accept the terms and privacy" register={register} />
             </DivCheck>
+            <Birthday register={register}/>
             <DivBtn>
                 <Button nameDiv="next" id="next" type="submit"  text="Next" imageTwo={Image} />
             </DivBtn>
--- form-challenge/src/Components/Forms/Basic/Basic.styled.js
+++ form-challenge/src/Components/Forms/Basic/Basic.styled.js
@@ -29,4 +29,5 @@ export const DivCheck = styled.div`
     flex-direction: column;
     align-items: flex-start;
     justify-content: flex-start;
+    margin-bottom: 2rem;
 `;
