0a6549812c6bc7fbdd353f59d37f322895367f4a
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-17 17:00:44 -0300

    Refactor: editing the navbar

---
+++ form-challenge/src/Components/Navbar/Navbar.styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const NavStyled = styled.div`
+  .navbar-div {
+    width: 515px;
+  }
+  .sucess {
+    display: none;
+  }
+  .nav {
+    display: flex;
+    flex-direction: row;
+  }
+  
+  .tab {
+    display: flex;
+    width: 100%;
+    justify-content: space-around;
+    border-bottom: 2px solid #AAAAAA;
+    cursor: pointer;
+    margin-bottom: 	2.1875rem;
+  }
+  #true {
+    p {
+      margin-bottom: 0.0625px;
+    }
+    border-bottom: 3px solid #074EE8;
+  }
+`;
