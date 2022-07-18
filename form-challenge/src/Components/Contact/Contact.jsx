c0cf72aded157ba57de74b6ab6e7f1c142d93b43
Joao Augusto <joaoaugustofrezende@outlook.com>
2022-07-18 16:29:57 -0300

    refactor: added onChange on phone input

--- form-challenge/src/Components/Contact/Contact.jsx
+++ form-challenge/src/Components/Contact/Contact.jsx
@@ -2,11 +2,11 @@ import {Input} from '../Input/Input'
 import { ContactDiv } from './Contact.styled';
 
 
-export const Contact = ({register, errors, text}) => {
+export const Contact = ({register, errors, text, onChange}) => {
     return (
         <ContactDiv>
            <Input id="email" type="email" errors={errors} text={text} label="Email *" placeholder="foor@bar.com" register={register} />
-           <Input id="phone" type="tel" errors={errors} text={text}  label="Phone" placeholder="(83) 00000-0000" register={register} />
+           <Input id="phone" type="tel" errors={errors} label="Phone" placeholder="(83) 00000-0000" register={register} onChange={onChange}/>
         </ContactDiv>
     );
 };
