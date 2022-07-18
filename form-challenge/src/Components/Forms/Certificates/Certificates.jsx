fb14a224f82d076fc05e5dc690b174f55eb7ca12
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-18 18:22:10 -0300

    Feat: creating the certificates form

---
+++ form-challenge/src/Components/Forms/Certificates/Certificates.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { CertificatesForm, DivBtn } from "./Certificates.styled";
+import {Graduation} from "../../Graduation/Graduation"
+import Button from '../../Button/Button';
+import Image from '../../../Assets/check.png';
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { AuthContext } from "../../Containers/Page/Page";
+//import schema from "../../../Utils/Validations";
+
+const Certificates = ({unLock}) => {
+    
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+    });
+
+    const { name } = React.useContext(AuthContext); //page authcontext
+    const [page,setPage] = name; //page state
+
+    let pageState;
+    const onSubmit = data => {
+    pageState=1;
+    setPage(pageState)
+    unLock(1)
+    console.log(data);
+    }
+    
+    return (
+        <CertificatesForm  onSubmit={handleSubmit(onSubmit)}>
+            <Graduation register={register} errors={errors} text="Please enter your Name"/>
+            <DivBtn>
+                <Button nameDiv="finish" id="finish" type="submit" text="Finish" image={Image} unLock={unLock}/>
+            </DivBtn>
+        </CertificatesForm>
+    );
+};
+
+export default Certificates
