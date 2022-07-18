fa69f3949b8c814bd89b3e45a5f6fbcdb3d19825
Joao Augusto <joaoaugustofrezende@outlook.com>
2022-07-18 17:47:31 -0300

    refactor: onChange added on develop

--- form-challenge/src/Components/Input/Input.jsx
+++ form-challenge/src/Components/Input/Input.jsx
@@ -4,11 +4,11 @@ import InputTag from '../Micro/InputTag/InputTag';
 import { InputContainer } from './Input.styled';
 import ErrorMessage from '../Micro/ErrorMessage/ErrorMessage';
 
-export const Input = ({id, label, type, placeholder, register, errors, text}) => {
+export const Input = ({id, label, type, placeholder, register, errors, text, onChange}) => {
     return (
         <InputContainer >
             <Label style="form" label={label}/> <ErrorMessage errors={errors} id={id} text={text}/>
-            <InputTag style="form" id={id} type={type} placeholder={placeholder} register={register}/>
+            <InputTag style="form" id={id} type={type} placeholder={placeholder} register={register} onChange={onChange}/>
         </InputContainer>
     );
 };
