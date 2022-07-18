0bf678d9cc7c350adda15c6a66e0d4f2bee4d39a
Joao Augusto <joaoaugustofrezende@outlook.com>
2022-07-18 17:44:45 -0300

    refactor: error message changed

--- form-challenge/src/Components/Forms/Social/Social.jsx
+++ form-challenge/src/Components/Forms/Social/Social.jsx
@@ -1,15 +1,34 @@
+import React from "react";
 import { useForm } from  "react-hook-form";
 import {Input} from '../../Input/Input';
 import { SocialForm, DivBtn } from "./Social.styled";
 import Button from '../../Button/Button';
 import Image from '../../../Assets/right-arrow.png'
 
-const Social = () => {
-    const { register, handleSubmit, formState: { errors }, reset } =  useForm();
+import { yupResolver } from '@hookform/resolvers/yup';
+import { AuthContext } from "../../Containers/Page/Page";
+//import schema from "../../../Utils/Validations";
+
+const Social = ({unLock}) => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
+    
+    const { name } = React.useContext(AuthContext); //page authcontext
+    const [page,setPage] = name; //page state
+
+    let pageState;
+    const onSubmit = data => {
+    pageState=1;
+    setPage(pageState)
+    unLock(1)
+    console.log(data);
+    }
+
     return (
-        <SocialForm>
-            <Input id="linkedin" label="LindedIn" type="text" placeholder="https//www.linkedin.com/in/foo-bar-3a0560104/" register={register} />
-            <Input id="github" label="Github *" type="text" placeholder="http://github.com/foobar" register={register} />
+        <SocialForm onSubmit={handleSubmit(onSubmit)}>
+            <Input id="linkedin" errors={errors} text="Please enter your Name" label="LindedIn" type="text" placeholder="https//www.linkedin.com/in/foo-bar-3a0560104/" register={register} />
+            <Input id="github" errors={errors} text="Please enter your Name" label="Github *" type="text" placeholder="http://github.com/foobar" register={register} unLock={unLock} />
             <DivBtn>
                 <Button nameDiv="next" id="next" type="submit"  text="Next"  imageTwo={Image}/>
             </DivBtn>
