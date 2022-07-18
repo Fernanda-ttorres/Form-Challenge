00a5acd29787051fdd1de72b1fa124bdac43bb19
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-18 10:26:52 -0300

    Feat: adding the birthday text

--- form-challenge/src/Components/Forms/Basic/Basic.jsx
+++ form-challenge/src/Components/Forms/Basic/Basic.jsx
@@ -5,6 +5,7 @@ import { BasicForm, DivBtn, DivCheck } from "./Basic.styled";
 import Button from '../../Button/Button';
 import Image from '../../../Assets/right-arrow.png';
 import Birthday from '../../Birthday/Birthday';
+import Text from "../../Micro/Text/Text";
 
 const Basic = () => {
     
@@ -15,6 +16,7 @@ const Basic = () => {
             <DivCheck>
                 <Checkbox id="checkbox" type="checkbox" label="I accept the terms and privacy" register={register} />
             </DivCheck>
+            <Text container="birthday" text="Birthday *"/>
             <Birthday register={register}/>
             <DivBtn>
                 <Button nameDiv="next" id="next" type="submit"  text="Next" imageTwo={Image} />
